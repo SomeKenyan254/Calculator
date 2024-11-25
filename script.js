e76bdf75fc85fb677966ef1acabccd9f63305b4b
Arnold Korir <144571592+SomeKenyan254@users.noreply.github.com>
2024-11-25 07:24:05 -0700

    Implimented a function to populate the screen

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const divide = (a,b) => (a / b)
 let num1 = "";
 let num2 = "";
 let operator = "";
-
+let displayNum = ""
 
 //Function that calls on the  operation
-const operate = function(){
+const operate =() => {
     if (operator == "+" ){
      return sum(num1,num2)
     }
@@ -29,3 +29,21 @@ const operate = function(){
         return
     }
 }
+
+
+const screen = document.querySelector(".display")
+
+let newNum = (value) => {
+
+    displayNum += value //apend to current display value
+
+  screen.textContent = displayNum //update with new value
+}
+const btn = document.querySelectorAll(".buttons");
+ btn.forEach(button => {
+    button.addEventListener("click",(e) =>{
+    const value = e.target.id
+    newNum(value)
+
+   })
+ })
